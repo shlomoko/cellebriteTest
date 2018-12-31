b586efbb615b2ce6f79d8be64d8e06228940f7dd
Shlomo <shlomokoppel2@gmail.com>
2018-12-31 19:42:48 +0200

    prettify()

--- main.py
+++ main.py
@@ -10,33 +10,37 @@ class TestParser:
     def __init__(self, filePath):
         self.filePath = filePath
 
-    def parse_file(self):
+    def parseFile(self):
         with open(self.filePath, 'rb') as f:
 
             for line in f:
-                decodedLine = line.decode('utf_8', 'ignore').replace('\xa0', " ").strip()[4:]
+                decodedLine = line.decode('utf_8', 'ignore').strip()[4:]
                 ids = re.findall(r'(.{4}000.)', decodedLine)
                 values = re.split(r'.{4}000.{2}', decodedLine)[1:]
-                lineDf = pd.DataFrame(data=[ids, values]).T
-                lineDf.columns = ['ids', 'values']
-                self.df = pd.merge(self.df, lineDf, on=['ids'], how='outer')
-            # contents = f.readline().decode('utf_8', 'ignore')
-            # for line in contents:
-            #     print ('line: ' + line)
-            #
+                self.appendDf(ids, values)
+
+            self.cleanDf()
+
+    def appendDf(self, ids, values):
+        lineDf = pd.DataFrame(data=[ids, values]).T
+        lineDf.columns = ['ids', 'values']
+        self.df = pd.merge(self.df, lineDf, on=['ids'], how='outer')
+
+    def fromEpochToDate(self, epoch):
+        if pd.isnull(epoch): return '-'
+        else: return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(epoch))
+
+    def cleanDf(self):
+        self.df = self.df.dropna(axis=1, how='all')
+        del self.df['ids']
+        self.df.columns = ['first_name', 'last_name', 'phone', 'date']
+        self.df['date'] = self.df['date'].astype(float).apply(self.fromEpochToDate)
 
 
-def fromEpochToDate(epoch):
-    if pd.isnull(epoch): return '-'
-    else: return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(epoch))
 
-a = '/Users/shlomokoppel/Downloads/ex_v7'
+a = '/ex_v7'
 parser = TestParser(a)
-parser.parse_file()
-parser.df = parser.df.dropna(axis=1, how='all')
-del parser.df['ids']
-parser.df.columns = ['first_name','last_name','phone','date']
-parser.df['date'] = parser.df['date'].astype(float).apply(fromEpochToDate)
+parser.parseFile()
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):
     print(parser.df)
 # print(parser.df.value)
